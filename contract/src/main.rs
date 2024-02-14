dcce07b2afb0fc84f42068e4c969b64f10bbb8fd
mikesully893 <msully893@gmail.com>
2024-02-14 19:20:15 +0000

    initial commit with project setup and start of main.rs in contract

---
+++ contract/src/main.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
+
+use casper_contract::{
+    contract_api::{runtime, storage},
+    unwrap_or_revert::UnwrapOrRevert,
+};
+
+use casper_types::{
+    api_error::ApiError,
+    contracts::{EntryPoint, EntryPointAccess, EntryPointType, EntryPoints, NamedKeys},
+    CLType, CLValue, URef,
+};
